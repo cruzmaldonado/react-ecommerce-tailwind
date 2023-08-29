479d3de3eede742ce70ec37e9c5a678015f3956b
Cruz Maldonado <cruzmaldonadocharris@gmail.com>
2023-08-29 05:50:19 -0400

    acomodando el commit aterior

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -7,15 +7,27 @@ function Home() {
   const [items, setItems] = useState(null)
   const URL ='https://api.escuelajs.co/api/v1/products'
   useEffect(() => {
-    
+    fetch(URL)
+    .then(response =>response.json())
+    .then(data=> setItems(data))
+   
     
   }, [])
-  
+
 
   return (
     <Layout>
       Home 
-      
+      <div className="grid gap-2 grid-cols-4 w-full max-w-screen-lg">
+
+      {
+        items?.map(item=>(
+          <Card key ={item.id} item={item}/>
+          
+          )
+          )
+        }
+      </div>
         <ProductDetail/>
     </Layout>
     
