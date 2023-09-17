47d356d46f97a4542a9808def3f15e408a699b2e
Cruz Maldonado <106775906+cruzmaldonado@users.noreply.github.com>
2023-09-17 12:34:18 -0400

    Creando pagina 404

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -5,7 +5,11 @@ function NotFound() {
 
     return (
       <Layout> 
-      Not Found 404
+     <div className='flex flex-col justify-center items-center'>
+        estas en 404
+        <h1 className='text-white  text-3xl text-center'>Esta pagina no existe</h1>
+        <img src="https://midu.dev/images/this-is-fine-404.gif" alt="Pagina no existe" />
+    </div>
       </Layout>   
     )
   }
